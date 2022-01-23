482136d5f22741706211f4d5ac2e750d66532789
isnafatchiyah <96872858+isnafatchiyah@users.noreply.github.com>
2022-01-23 12:32:20 +0700

    Add files via upload

---
+++ error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Belajar PHP</title>
+</head>
+<body>
+
+<h1>Belajar PHP</h1>
+
+ <?php 
+ // echo print
+ // print_r die var_dump
+ 
+ 
+ $car = ['mobil', 'motor'];
+ var_dump($car);
+ 
+ ?>
+ </body>
+ </html>
